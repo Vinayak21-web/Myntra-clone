35e81f8c5e9725f6819b256399e443e62b85e3e0
Vinayak jk <vinayakkodiya193@gmail.com>
2025-04-29 22:52:01 +0530

    Add files via upload

---
+++ vinayak/02-js/script/02.js
@@ -0,0 +1,39 @@
+console.log("vinayak jk");
+
+let v=50
+console.log(v);
+
+var num=50
+console.log(typeof(num));
+console.log(num);
+
+var name="vinu"
+console.log(name);
+var name='vinayak jk'
+console.log(name);
+
+const a=47
+console.log(a); 
+console.log(typeof(name));
+
+let one=true
+console.log(one);
+console.log(typeof(one));
+
+let two=null
+console.log(typeof(two));
+
+
+let z=10;
+let b=20
+
+console.log("addition of two number: ",(z+b))
+console.log("substraction of two number: ",(z-b))
+console.log("multiplication of two number: ",(z*b)) 
+console.log("division of two number: ",(z/b))
+console.log("modules of two number: ",(z%b))
+console.log("power  of two number: ",(z**b))
+
+let arr=[1,2,3,4,5,6,7];
+console.log(arr);
+console.log(typeof(arr));
